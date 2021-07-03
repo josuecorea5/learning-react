a80382d41cb69cf67a9220c3f1b2364e3d086809
DCoreas <josuepaiz487@gmail.com>
2021-07-03 14:45:39 -0600

    Add event to Component

--- Avatar.js
+++ Avatar.js
@@ -6,3 +6,29 @@ const Avatar = (params) => {
 };
 
 ReactDOM.render(e(Avatar, { id: 20, gender: "women" }), app);
+
+
+//Component using jsx
+const Avatar = (params) => {
+  const src = `https://randomuser.me/api/portraits/${params.gender}/${params.id}.jpg`;
+
+  return (
+    <picture>
+      <img
+        onClick={(event) => {
+          event.target.classList.toggle("disabled");
+        }}
+        src={src}
+      />
+      <em>{params.name}</em>
+    </picture>
+  );
+};
+
+ReactDOM.render(
+  <div>
+    <Avatar id={10} gender={"women"} />
+    <Avatar id={20} gender={"men"} />
+  </div>,
+  app
+);
